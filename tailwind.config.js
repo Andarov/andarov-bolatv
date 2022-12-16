34702b8446403854e2fcebd9075b41ae6c8a9a7a
Andarov <bekturandarob678@gmail.com>
2022-12-16 14:10:24 +0500

    Add font family and some brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        signika: ["Signika Negative", "sans-serif"],
+      },
+      colors:{
+        br:{
+          'red': "#a30015;",
+          'blue': "#319437",
+          'violet': '#5b2269',
+          'darkBlue': '#004686'
+        }
+      },
+      maxWidth:{
+        "base": "1240px",
+      }
+    },
   },
   plugins: [],
 }
