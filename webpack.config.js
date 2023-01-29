10e3e20ecda2f79d858ab011b79e426f6205db8e
Andarov <bekturandarob678@gmail.com>
2023-01-29 23:33:44 +0500

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -83,5 +83,15 @@ module.exports = {
                 ]
               }
         ]
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+      },
 }
