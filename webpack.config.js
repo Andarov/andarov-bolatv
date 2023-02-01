a282fd27994a590c616cd8d3295147743862958a
Andarov <bekturandarob678@gmail.com>
2023-02-01 09:57:29 +0500

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,19 @@ module.exports = {
                     }
                   }
                 ]
-              }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            }
         ]
     },
     devServer: {
