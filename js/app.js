1b6eaa2c3f7cfd5240b5c0b7a6ca0ab4e49b53dc
Andarov <bekturandarob678@gmail.com>
2023-05-29 17:10:45 +0500

    Format document js files

--- js/app.js
+++ js/app.js
@@ -1,56 +1,19 @@
 // Import files
-import "../dist/output.css"
+import "../dist/output.css";
 import { bolatv } from "./api.js";
-import { displayVideos } from "./displayVideos.js"
+import { displayVideos } from "./displayVideos.js";
 
 // When the user scrolls down 20px from the top of the document, show the button
 let mybutton = document.getElementById("btn-back-to-top");
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-
-function add_video(url,element) {
-  var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    
-        var videoID = url;
-    
-        var playerDiv = document.createElement('div');
-        playerDiv.id = 'player';
-        element.appendChild(playerDiv);
-    
-        var player;
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-                height: '390',
-                width: '640',
-                videoId: videoID,
-                events: {
-                    'onStateChange': onPlayerStateChange
-                }
-            });
-        }
-    
-        function onPlayerStateChange(event) {
-            if (event.data == YT.PlayerState.ENDED) {
-                event.target.seekTo(0);
-                event.target.playVideo();
-            }
-        }
-}
-
-
-scrollFunction()
+scrollFunction();
 window.onscroll = function () {
   scrollFunction();
 };
@@ -131,35 +94,70 @@ if ((uzbTaleMob, worldTaleMob)) {
 
 // Display multic videos
 const pomidorMob = document.querySelector("#pomidor-mob");
-const avtoMob = document.querySelector('#avto-mob');
-const pandaMob = document.querySelector('#panda-mob');
-const qunduzMob = document.querySelector('#qunduz-mob');
-const mittiMob = document.querySelector('#mitti-mob');
-const nussaMob = document.querySelector('#nussa-mob');
-const mashaMob = document.querySelector('#masha-mob');
-const multicMob = document.querySelector('#multic-mob');
-if (pomidorMob, avtoMob, pandaMob, qunduzMob, nussaMob, mashaMob, multicMob) {
+const avtoMob = document.querySelector("#avto-mob");
+const pandaMob = document.querySelector("#panda-mob");
+const qunduzMob = document.querySelector("#qunduz-mob");
+const mittiMob = document.querySelector("#mitti-mob");
+const nussaMob = document.querySelector("#nussa-mob");
+const mashaMob = document.querySelector("#masha-mob");
+const multicMob = document.querySelector("#multic-mob");
+if ((pomidorMob, avtoMob, pandaMob, qunduzMob, nussaMob, mashaMob, multicMob)) {
   displayVideos(pomidorMob, bolatv.cartoon[0]);
-  displayVideos(avtoMob, bolatv.cartoon[1])
-  displayVideos(pandaMob, bolatv.cartoon[2])
-  displayVideos(qunduzMob, bolatv.cartoon[3])
-  displayVideos(mittiMob, bolatv.cartoon[4])
-  displayVideos(nussaMob, bolatv.cartoon[5])
-  displayVideos(mashaMob, bolatv.cartoon[6])
-  displayVideos(multicMob, bolatv.cartoon[7])
+  displayVideos(avtoMob, bolatv.cartoon[1]);
+  displayVideos(pandaMob, bolatv.cartoon[2]);
+  displayVideos(qunduzMob, bolatv.cartoon[3]);
+  displayVideos(mittiMob, bolatv.cartoon[4]);
+  displayVideos(nussaMob, bolatv.cartoon[5]);
+  displayVideos(mashaMob, bolatv.cartoon[6]);
+  displayVideos(multicMob, bolatv.cartoon[7]);
 }
 
 // Display songs videos
-const uzbSongMob = document.querySelector('#uzb-song-mob')
-const engSongMob = document.querySelector('#eng-song-mob')
-if(uzbSongMob){
-  displayVideos(uzbSongMob, bolatv.songs[0])
-  displayVideos(engSongMob, bolatv.songs[1])
+const uzbSongMob = document.querySelector("#uzb-song-mob");
+const engSongMob = document.querySelector("#eng-song-mob");
+if (uzbSongMob) {
+  displayVideos(uzbSongMob, bolatv.songs[0]);
+  displayVideos(engSongMob, bolatv.songs[1]);
+}
+
+function add_video(url, element) {
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var videoID = url;
+
+  var playerDiv = document.createElement("div");
+  playerDiv.id = "player";
+  element.appendChild(playerDiv);
+
+  var player;
+
+  // onYouTubeIframeAPIReady funksiyasini o'zgartiring
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player("player", {
+      height: "390",
+      width: "640",
+      videoId: videoID,
+      events: {
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  }
+
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+      event.target.seekTo(0);
+      event.target.playVideo();
+    }
+  }
 }
 
 // Iframe yaratish
 const parseMediaURL = (media) => {
-  const regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  const regexp =
+    /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
   const url = media.src;
   const match = url.match(regexp);
   return match ? match[1] : match;
@@ -169,7 +167,10 @@ const createIframe = (id) => {
   const iframe = document.createElement("iframe");
   iframe.setAttribute("allowfullscreen", "");
   iframe.setAttribute("allow", "autoplay");
-  iframe.setAttribute("src", `https://www.youtube.com/embed/${id}?playlist=${id}&loop=1&autoplay=1`);
+  iframe.setAttribute(
+    "src",
+    `https://www.youtube.com/embed/${id}?playlist=${id}&loop=1&autoplay=1`
+  );
   iframe.classList.add("w-full", "h-full");
   return iframe;
 };
@@ -190,14 +191,13 @@ const setupVideo = (video) => {
   const media = video.querySelector("#video-img");
   const id = parseMediaURL(media);
 
-  
   let iframe;
 
   video.addEventListener("click", () => {
     modalWrapper.classList.remove("hidden");
     modal.classList.remove("hidden");
     iframe = createIframe(id);
-    add_video(id,iframe)
+    add_video(id, iframe);
 
     modal.prepend(iframe);
   });
@@ -220,7 +220,6 @@ const setupVideo = (video) => {
 };
 findVideos();
 
-
 // Nomi bilan qidiruv
 const elForm = document.querySelector("#form");
 // Funksiya
@@ -228,7 +227,7 @@ const searchName = function () {
   // Video nomini olish
   videos.forEach(function (video) {
     const videoTitle = video.querySelectorAll("#video-title");
-    
+
     // Agar input valuesi video nomiga teng kelsa uni chiqarish qolganini ochirish
     elForm["name-input"].addEventListener("input", function () {
       const inputNameValue = form["name-input"].value.toLowerCase();
@@ -266,27 +265,26 @@ const elSelect = document.querySelector("#select");
 elSelect.addEventListener("change", function () {
   videos.forEach(function (video) {
     const videoLang = video.querySelectorAll("#video-lang");
-    
+
     videoLang.forEach(function (lang) {
       if (lang.textContent.includes(elSelect.value)) {
         lang.parentElement.classList.remove("hidden");
       } else {
         lang.parentElement.classList.add("hidden");
       }
-      if(elSelect.value == 'all'){
+      if (elSelect.value == "all") {
         lang.parentElement.classList.remove("hidden");
       }
     });
   });
 });
 
-
 // Swipper
 const uzbErtak = new Swiper(".uzb-tale-mob", {
   grabCursor: true,
   slidesPerView: 1.2,
   spaceBetween: 10,
-  
+
   navigation: {
     nextEl: ".swiper-button-next-1",
     prevEl: ".swiper-button-prev-1",
@@ -297,7 +295,7 @@ const jahonErtak = new Swiper(".world-tale-mob", {
   grabCursor: true,
   slidesPerView: 1.2,
   spaceBetween: 10,
-  
+
   navigation: {
     nextEl: ".swiper-button-next-2",
     prevEl: ".swiper-button-prev-2",
@@ -403,4 +401,3 @@ const engSongSlider = new Swiper(".eng-song-mob", {
     prevEl: ".swiper-button-prev-12",
   },
 });
-
